223214717ed4a3fcbb9ade1d4de49cb946933415
Mateusz Kośmiński <kosminskimateusz@gmail.com>
2021-10-08 08:59:54 +0200

    Changed Connection String to database for Windows

--- Dietownik.DataAccess/RecipesStorageContextFactory.cs
+++ Dietownik.DataAccess/RecipesStorageContextFactory.cs
@@ -8,7 +8,8 @@ namespace Dietownik.DataAccess
         public RecipeStorageContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<RecipeStorageContext>();
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=RecipeStorage;User=SA;Password=Strabane2020;");
+            optionsBuilder.UseSqlServer("Data Source=M-KOSMINSKI\\KOSMI;Initial Catalog=Dietownik;Integrated Security=True");
+            // Linux: Data Source=localhost;Initial Catalog=RecipeStorage;User=SA;Password=Strabane2020;
             return new RecipeStorageContext(optionsBuilder.Options);
         }
     }
