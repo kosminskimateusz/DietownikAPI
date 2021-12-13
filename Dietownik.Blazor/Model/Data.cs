4efb3ef942f1577524f2bde104c8ced57a723cf2
Mateusz Kośmiński <kosminskimateusz@gmail.com>
2021-12-13 19:18:08 +0100

    Easy connection to API - Succed

---
+++ Dietownik.Blazor/Model/Data.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dietownik.Blazor.Model
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        public decimal Kcal { get; set; }
+        public decimal Fats { get; set; }
+        public decimal Carbs { get; set; }
+        public decimal Proteins { get; set; }
+
+    }
+}
+
+// {"data":[{"name":"Papryka","kcal":10.0,"fats":0.0,"carbs":8.0,"proteins":0.0,"id":1}],"error":null}
--- Dietownik.Blazor/Program.cs
+++ Dietownik.Blazor/Program.cs
@@ -1,20 +1,11 @@
 using Dietownik.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-
-//builder.Services.AddScoped(x => {
-//    var apiUrl = new Uri("https://localhost:44371");
-//    return new HttpClient() { BaseAddress = apiUrl };
-//});
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44371/") });
-
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
